d182d6f2afe3593b8b7aea97d43868bd1bdab731
Stefan F <focastefan13@gmail.com>
2025-10-19 14:44:40 +0300

    Update animepaheauto.js

--- animepaheauto.js
+++ animepaheauto.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Merlyin
-// @match        https://animepahe.com/play/*
+// @match        https://animepahe.si/play/*
 // @match        https://kwik.cx/e/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=animepahe.com
 // @grant        GM_setValue
@@ -14,28 +14,24 @@
 (function() {
     'use strict';
 
-    // Your code here...
-
     let nextEpisodeLink = "";
     let currentEpisodeNumber = "";
     let lastEpisodeNumber = "";
     let progressBar;
     let videoPlayer;
     let continueAutoPlaying;
+    let stillOnAnimePaheAndNotOnKwik = true;
+    let theButton;
 
 //------------------------------animepahe---------------------------------------\\
-    function clickTheLoadingButton() {
-        document.querySelector('.click-to-load').click();
-    }
 
     function openTheVideo() {
-        window.location.href = document.querySelector('.embed-responsive-item').src;
+        window.location.href = document.querySelectorAll('head > script')[1].textContent.split("\n")[3].split('"')[1];
     }
 
     function getNextEpisode() {
         if(currentEpisodeNumber != lastEpisodeNumber) {
             nextEpisodeLink = document.querySelector('.sequel a').href;
-            console.log(nextEpisodeLink);
             GM_setValue("nextEp", nextEpisodeLink);
         }
     }
@@ -58,10 +54,8 @@
     function getVideoAndProgressBar() {
         videoPlayer = document.querySelector('#kwikPlayer');
         progressBar = document.querySelector('input');
-        document.querySelector('button[aria-label="Play"]').click()
+        document.querySelector('button[aria-label="Play"]').click();
     }
-
-    //function makeItFullScreen() { document.querySelector("button[data-plyr='fullscreen']").click(); }
 
     function hasItReachedTheEnd() {
         return progressBar.ariaValueNow == progressBar.ariaValueMax && progressBar.ariaValueMax != 0;
@@ -74,48 +68,89 @@
         } else {
             window.location.href = GM_getValue("animeInfo", "/");
         }
-        //window.location.href = nextEpisodeLink;
-        //console.log("hello");
-        //console.log(nextEpisodeLink);
+    }
+
+    function updateButtonText() {
+        if(GM_getValue("keepPlaying", true)) {
+            theButton.textContent = "Autoplay: On";
+        } else {
+            theButton.textContent = "Autoplay: Off";
+        }
+    }
+
+    function onOffButton() {
+
+        theButton = document.createElement("button");
+        theButton.style.position = 'absolute';
+        theButton.style.zIndex = "50";
+
+        updateButtonText();
+
+        //make button hidden while video is playing
+        if(!GM_getValue("stillOnAnimePaheAndNotOnKwik", false)) {
+            var interval = setInterval(function() {if(document.querySelector(".plyr--hide-controls") === null) {theButton.hidden = false;} else {theButton.hidden = true;} }, 500);
+        }
+
+        theButton.onclick = function() {
+            if(GM_getValue("keepPlaying", false)) {
+
+                GM_setValue("keepPlaying", !GM_getValue("keepPlaying", false));
+                updateButtonText();
+            } else {
+
+                GM_setValue("keepPlaying", !GM_getValue("keepPlaying", false));
+                updateButtonText();
+
+                if(GM_getValue("stillOnAnimePaheAndNotOnKwik", false) && (window.location.href.indexOf("animepahe") != -1)) {
+
+                    GM_setValue("stillOnAnimePaheAndNotOnKwik", false);
+                    openTheVideo();
+                }
+            }
+        };
+
+        document.body.prepend(theButton);
+
     }
 //---------------------------------runtime-------------------------------------------\\
 
     function keepPlaying() {
-        //GM_setValue("keepPlaying", false);
         return GM_getValue("keepPlaying", continueAutoPlaying);
     }
 
     function startOp() {
-        console.log("hello from the outside of the if");
         if(window.location.href.indexOf("animepahe") != -1){
-            console.log("hello from the place you thought you were in");
-            clickTheLoadingButton();
+            GM_setValue("stillOnAnimePaheAndNotOnKwik", true);
+            onOffButton();
             getCurrentEpisodeNumber();
             getLastEpisodeNumber();
             getAnimeInfoPage();
             getNextEpisode();
             if(keepPlaying()) {
+                GM_setValue("stillOnAnimePaheAndNotOnKwik", false);
                 openTheVideo();
             }
         } else {
-            getVideoAndProgressBar();
-            console.log("hello from the place you thought you weren't going to reach " + window.location.href);
-            //getAndStartVideo();
-            //makeItFullScreen();
-            //console.log("hi");
-            var interval = setInterval(function() {if(hasItReachedTheEnd() == true) {goToNextVideo()} }, 1000);
+            if(!GM_getValue("stillOnAnimePaheAndNotOnKwik", false)) {
+                onOffButton();
+                getVideoAndProgressBar();
+                var interval = setInterval(function() {if(hasItReachedTheEnd() == true) {goToNextVideo()} }, 1000);
+            }
         }
     }
+
     document.body.onkeydown = function(event){
         event = event || window.event;
         var keycode = event.charCode || event.keyCode;
         if(keycode === 'G'.charCodeAt()){
             videoPlayer.plyr.forward(85); }
         if(keycode === 'H'.charCodeAt()){
-           alert("howdy there, current episode is... " + GM_getValue("currentEpNum", "idk") + " out of " + GM_getValue("lastEpNum", "idk")); }
+           alert("You're on " + GM_getValue("currentEpNum", "idk") + " out of " + GM_getValue("lastEpNum", "idk") + "."); }
         if(keycode === 'Q'.charCodeAt()){
             GM_setValue("keepPlaying", !GM_getValue("keepPlaying", false));
+            updateButtonText();
             if(window.location.href.indexOf("animepahe") != -1) {
+                GM_setValue("stillOnAnimePaheAndNotOnKwik", false);
                 openTheVideo();
             }
         }
